f8520f4e8f22de497e5967cfe5c65491643ba743
vinicius <viniciusamalia@gmail.com>
2020-05-24 00:43:29 -0300

    meu site

---
+++ js/main.js
@@ -0,0 +1,11 @@
+(function(){
+    
+var carousel = new Carousel({
+        container: '.laptop-slider .slideshow',
+        itens: 'figure',
+        btnPrev: '.prev',
+        btnNext: '.next'
+    
+})
+   
+})()
